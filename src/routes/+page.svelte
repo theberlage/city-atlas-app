1045a71cf3e57025561b1ee3a218317e6bd947a4
Jules Schoonman <jules.schoonman@gmail.com>
2023-10-27 09:01:15 +0200

    removed hex function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,27 +71,6 @@
 		return fetch(url).then((response) => response.json())
 	}
 
-	// From: https://gist.github.com/danieliser/b4b24c9f772066bcf0a6
-
-	function convertHexToRGBA(hexCode, opacity = 1) {
-		let hex = hexCode.replace('#', '')
-
-		if (hex.length === 3) {
-			hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`
-		}
-
-		const r = parseInt(hex.substring(0, 2), 16)
-		const g = parseInt(hex.substring(2, 4), 16)
-		const b = parseInt(hex.substring(4, 6), 16)
-
-		/* Backward compatibility for whole number based opacity values. */
-		if (opacity > 1 && opacity <= 100) {
-			opacity = opacity / 100
-		}
-
-		return `rgba(${r},${g},${b},${opacity})`
-	}
-
 	// Function to add vector layer
 
 	async function addVectorSource(geojsonsPaths: Array<string>) {
